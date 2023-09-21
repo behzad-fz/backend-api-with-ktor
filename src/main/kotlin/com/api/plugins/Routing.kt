0844986ff95730e80fed5198c6a19b8d79c1d3da
Behzad <behzad.fazelasl@gmail.com>
2023-09-21 23:16:33 +0200

    create a new item

--- src/main/kotlin/com/api/plugins/Routing.kt
+++ src/main/kotlin/com/api/plugins/Routing.kt
@@ -1,13 +1,26 @@
 package com.api.plugins
 
+import com.api.model.Item
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+
+val items = mutableListOf(Item(1, "Item 1"), Item(2, "Item 2"))
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        route("/items") {
+            post {
+                val newItem = call.receive<Item>()
+                com.api.plugins.items.add(newItem)
+                call.respond(HttpStatusCode.Created, newItem)
+            }
         }
     }
 }
